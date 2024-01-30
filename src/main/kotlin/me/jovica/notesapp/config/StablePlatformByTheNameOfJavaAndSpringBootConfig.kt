3eabe84cca7658e7eff60cd474a5ae4578eb9f46
Jovica Veljkovic <166545932+jovica-me@users.noreply.github.com>
2024-01-30 14:47:57 +0100

    Session not working

---
+++ src/main/kotlin/me/jovica/notesapp/config/StablePlatformByTheNameOfJavaAndSpringBootConfig.kt
@@ -0,0 +1,14 @@
+package me.jovica.notesapp.config
+
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.session.jdbc.MySqlJdbcIndexedSessionRepositoryCustomizer
+
+
+//@Configuration
+//class StablePlatformByTheNameOfJavaAndSpringBootConfig {
+//    @Bean
+//    fun sessionRepositoryCustomizer(): MySqlJdbcIndexedSessionRepositoryCustomizer {
+//        return MySqlJdbcIndexedSessionRepositoryCustomizer()
+//    }
+//}
